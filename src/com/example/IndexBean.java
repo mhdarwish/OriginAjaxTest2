df6de5b80586e5ad4aab246016eea2974352b74a
mhdarwish <m.darwish111000111@outlook.com>
2015-10-20 15:25:07 +0300

    second commit

--- src/com/example/IndexBean.java
+++ src/com/example/IndexBean.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
